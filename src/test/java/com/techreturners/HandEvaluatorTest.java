d29ea4a5de5fefbaa12ba87906cd181533e63a85
dwright1989 <debbiewright@live.co.uk>
2023-03-02 16:11:24 +0000

    More tests passing for high card e.g. when no high cards and when multiple high cards

--- src/test/java/com/techreturners/HandEvaluatorTest.java
+++ src/test/java/com/techreturners/HandEvaluatorTest.java
@@ -1,15 +1,12 @@
 package com.techreturners;
 
-
-import com.techreturners.EnumsAndConstants.CardSuit;
-import com.techreturners.EnumsAndConstants.CardValue;
-import org.junit.jupiter.api.BeforeEach;
+import com.techreturners.EnumsAndConstants.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HandEvaluatorTest {
 
@@ -26,6 +23,30 @@ public class HandEvaluatorTest {
                 new Card(CardValue.FIVE, CardSuit.DIAMONDS))));
         Card card = HandEvaluator.isHighCard(hand);
         assertEquals(card.getValue(), CardValue.ACE);
+    }
+
+    @Test
+    public void willReturnZeroHighCards(){
+        Hand hand = new Hand(new ArrayList<>(Arrays.asList(
+                new Card(CardValue.FIVE, CardSuit.SPADES),
+                new Card(CardValue.EIGHT, CardSuit.HEARTS),
+                new Card(CardValue.TEN, CardSuit.SPADES),
+                new Card(CardValue.TWO, CardSuit.DIAMONDS),
+                new Card(CardValue.FIVE, CardSuit.DIAMONDS))));
+        Card card = HandEvaluator.isHighCard(hand);
+        assertNull(card);
+    }
+
+    @Test
+    public void willReturnHighestCard(){
+        Hand hand = new Hand(new ArrayList<>(Arrays.asList(
+                new Card(CardValue.QUEEN, CardSuit.SPADES),
+                new Card(CardValue.KING, CardSuit.HEARTS),
+                new Card(CardValue.TEN, CardSuit.SPADES),
+                new Card(CardValue.TWO, CardSuit.DIAMONDS),
+                new Card(CardValue.FIVE, CardSuit.DIAMONDS))));
+        Card card = HandEvaluator.isHighCard(hand);
+        assertEquals(card.getValue(), CardValue.KING);
     }
 
 }
